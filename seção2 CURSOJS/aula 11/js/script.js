f7f5dc6bb481344f76b9ee2fff1aa7137873073d
AndradeItalo <ifa2@cin.ufpe.br>
2023-11-08 09:56:33 -0300

    Feat: Atualizando seção 2 do cursoJS

---
+++ seção2 CURSOJS/aula 11/js/script.js
@@ -0,0 +1,7 @@
+//alert('Mensagem');
+let num1 = prompt('numero 1')
+let num2 = prompt('numero 2')
+console.log(num1, num2);
+
+let resultado = Number(num1) + Number(num2);
+alert(`o resultado foi ${resultado}` );
